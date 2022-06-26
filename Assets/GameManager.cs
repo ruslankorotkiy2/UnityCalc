461ddfef5261cbdaed56c11ac97997a00d475d51
StingerKR <89703505+StingerKR@users.noreply.github.com>
2022-06-26 19:36:11 +0300

    first commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    public TextField MyInput;
+    public int count;
+
+    //private void Start
+    
+    public void ButClick()
+    {
+        //MyInput.value = "test";
+        Debug.Log("test");
+        /*
+        Text text = GetComponent<Input>();
+        text.text = "your text";
+
+
+        */
+    }
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class TestScript : MonoBehaviour
+{
+    public Text MyText;
+
+    void Update()
+    {
+        MyText.text = "deer";
+    }
+}
